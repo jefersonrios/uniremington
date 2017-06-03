fda830bcca3b5ee23db71cbc1c2d28f961a295c8
jeferson <jefersonriosgallego@hotmail.com>
2017-06-03 16:25:31 -0500

    se creo la interface de usuario y los spinner

---
+++ app/src/main/java/com/example/user/uniremington/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.user.uniremington;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button ingresar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ingresar = (Button) findViewById(R.id.ingresar);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText) findViewById(R.id.email)).getText().toString();
+                String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                if (email.equals("jriosg@uniremingtonmanizales") && password.equals("1053790188"))
+                {
+                    Intent Nuevoform = new Intent(MainActivity.this,Main2Activity.class);
+                    startActivity(Nuevoform);
+
+                    Toast.makeText(ingresar.getContext(),"bienvenido",Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    Toast.makeText(ingresar.getContext(),"usuario y/o contraseña incorrectos",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
+}
